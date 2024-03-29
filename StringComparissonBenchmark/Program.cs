394a80a3bfd08087d03be16843f4cb2ec21914a4
Gerson Azabache <gerson_dunk@hotmail.com>
2024-03-29 09:35:59 -0500

    initial

---
+++ StringComparissonBenchmark/Program.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace ConsoleApp12
+{    
+    [MemoryDiagnoser]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<Program>();
+        }
+        [Benchmark]        
+        public bool CompasissonWithUpper()
+        {
+            string parameter1 = "BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER";
+            string parameter2 = "BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPEr";
+            return parameter1.ToUpper() == parameter2.ToUpper();
+        }
+        [Benchmark]        
+        public bool CompasissonWithEquals()
+        {
+            string parameter1 = "BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER";
+            string parameter2 = "BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPER BRAVE DEVELOPEr";
+            return string.Equals(parameter1, parameter2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
